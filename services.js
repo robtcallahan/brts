050fe6da370976f346931a2015c2ae863827b305
Rob Callahan <robtcallahan@gmail.com>
2025-02-20 16:19:51 -0500

    new

--- services.js
+++ services.js
@@ -1,14 +1,15 @@
+
 console.log("loading");
 
-window.toggleDiv = function() {
-    let content = document.getElementById('service_details');
-    let arrow = document.getElementById('arrow');
-    if (content.style.display === 'none' || content.style.display === '') {
-        content.style.display = 'block';
-        arrow.textContent = '▼';
+function toggleDiv() {
+    let content = document.getElementById("service_details");
+    let arrow = document.getElementById("arrow");
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+        arrow.textContent = "▼";
     } else {
-        content.style.display = 'none';
-        arrow.textContent = '▶';
+        content.style.display = "none";
+        arrow.textContent = "▶";
     }
 }
 
